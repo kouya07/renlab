b776d9f2a8cfebfde249bdaac0af292c3d1d1b87
kouya007 <luglio.11m44@gmail.com>
2020-04-05 18:13:01 +0900

    test

--- script.js
+++ script.js
@@ -131,6 +131,30 @@ $(function() {
       const el = $('#' + id).find('video').get(0);
       el.srcObject = stream;
       el.play();
+
+            const video = document.getElementById('video');
+      const togglePipButton = document.getElementById('togglePipButton');
+
+      // Hide button if Picture-in-Picture is not supported or disabled.
+      togglePipButton.hidden = !document.pictureInPictureEnabled ||
+        video.disablePictureInPicture;
+
+      togglePipButton.addEventListener('click', function() {
+        // If there is no element in Picture-in-Picture yet, let’s request
+        // Picture-in-Picture for the video, otherwise leave it.
+        if (!document.pictureInPictureElement) {
+          video.requestPictureInPicture()
+          .catch(error => {
+            // Video failed to enter Picture-in-Picture mode.
+          });
+        } else {
+          document.exitPictureInPicture()
+          .catch(error => {
+            // Video failed to leave Picture-in-Picture mode.
+          });
+        }
+      });
+      
     });
 
     room.on('removeStream', function(stream) {
